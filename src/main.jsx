c138762d8626a8d5bfb51c35d1c914a081166f3c
Gmicheletti <guilherme_micheletti@hotmail.com>
2025-12-30 22:56:55 -0300

    ajuste serv

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import { Analytics } from "@vercel/analytics/next"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
 Analytics();
