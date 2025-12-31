16aeaf261f39bff01a8c2d71d7e3693883f14b98
Gmicheletti <guilherme_micheletti@hotmail.com>
2025-12-30 22:58:57 -0300

    ajuste serv

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import { Analytics } from "@vercel/analytics/next"
+import { inject } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
 )
-Analytics();
+inject();
